47ea203d8948f8fd1b3a6fad6192ba11da3007bc
Ryan Kurte <ryankurte@gmail.com>
2015-12-06 23:06:49 +1300

    Working on menu widget

--- library/include/widgets/menu_widget.h
+++ library/include/widgets/menu_widget.h
@@ -17,10 +17,12 @@ typedef int32_t (*menu_get_num_callback)(void *menu_layer, void* data);
 typedef int16_t (*menu_get_height_callback)(void *menu_layer, uint16_t section, void* data);
 
 typedef void (*header_draw_callback)(ugui_graphics_t *graphics_ctx, ugui_layer_t *layer, char* title);
-typedef void (*menu_draw_callback)(ugui_graphics_t *graphics_ctx, const ugui_layer_t cell_layer, uint16_t section_index, void* data);
+typedef void (*menu_draw_callback)(ugui_graphics_t *graphics_ctx, ugui_layer_t *layer, char* title, char* data);
+
+typedef void (*menu_get_data_cb)(void* menu_layer, int index, char* title, char* data);
 typedef void (*menu_select_callback)(void *menu_layer, ugui_menu_index_t *cell_index, void *callback_context);
 
-typedef struct ugui_menu_widget_callbacks_s {
+typedef struct ugui_menu_widget_data_callbacks_s {
 	menu_get_num_callback get_num_sections;
 	menu_get_num_callback get_num_rows;
 
@@ -28,16 +30,22 @@ typedef struct ugui_menu_widget_callbacks_s {
 	menu_get_height_callback get_cell_height;
 
 	menu_select_callback select;
+
+} ugui_menu_widget_data_callbacks_t;
+
+typedef struct ugui_menu_widget_draw_callbacks_s {
 	header_draw_callback draw_header;
 	menu_draw_callback draw_row;
-	
-} ugui_menu_widget_callbacks_t;
+
+} ugui_menu_widget_draw_callbacks_t;
 
 typedef struct ugui_menu_widget_s ugui_menu_widget_t;
 
 ugui_menu_widget_t *ugui_menu_widget_create(ugui_rect_t frame);
 
-void ugui_menu_widget_set_callbacks(ugui_menu_widget_t* menu_widget, ugui_menu_widget_callbacks_t* callbacks);
+void ugui_menu_widget_set_callbacks(ugui_menu_widget_t* menu_widget, ugui_menu_widget_data_callbacks_t* callbacks);
+
+void ugui_menu_widget_set_draw(ugui_menu_widget_t* menu_widget, ugui_menu_widget_draw_callbacks_t* callbacks);
 
 ugui_layer_t* ugui_menu_widget_get_layer(ugui_menu_widget_t* menu_widget);
 
--- library/source/widgets/menu_widget.c
+++ library/source/widgets/menu_widget.c
@@ -5,7 +5,8 @@
 
 struct ugui_menu_widget_s {
 	ugui_layer_t* widget_layer;
-	ugui_menu_widget_callbacks_t callbacks;
+	ugui_menu_widget_draw_callbacks_t draw_callbacks;
+	ugui_menu_widget_data_callbacks_t data_callbacks;
 	int selected;
 };
 
@@ -13,9 +14,14 @@ static void menu_widget_layer_update(ugui_layer_t* layer, void* graphics_ctx, vo
 {
 	ugui_menu_widget_t* menu_widget = (ugui_menu_widget_t*) layer_ctx;
 
-	menu_widget->callbacks.draw_header(graphics_ctx, layer, "Test title");
+	menu_widget->draw_callbacks.draw_header(graphics_ctx, layer, "Test title");
 
+	char* title;
+	char* data;
 
+	for(uint8_t i=0; i<menu_widget->data_callbacks.get_num_rows(); i++) {
+		menu_widget->data_callbacks.
+	}	
 }
 
 void ugui_menu_widget_basic_header_draw(ugui_graphics_t *graphics_ctx, ugui_layer_t *layer, char* title)
@@ -38,9 +44,36 @@ void ugui_menu_widget_basic_header_draw(ugui_graphics_t *graphics_ctx, ugui_laye
 	});
 }
 
-void ugui_menu_widget_basic_cell_draw(ugui_graphics_t* grapics_context, ugui_layer_t* layer, char* title , char* data)
+void ugui_menu_widget_basic_cell_draw(ugui_graphics_t* graphics_ctx, ugui_layer_t* layer, char* title , char* data)
 {
+	ugui_rect_t* bounds = ugui_layer_get_bounds(layer);
 
+	int w = bounds->w;
+	int h = bounds->h;
+
+	ugui_size_t title_size;
+	ugui_font_get_text_size(&font_robotomono_regular_18,
+	                        title,
+	                        &title_size);
+
+	ugui_graphics_draw_text(graphics_ctx,
+	                        title,
+	                        &font_robotomono_regular_18,
+	(ugui_point_t) {
+		.x = w / 2 - title_size.w / 2, .y = 0
+	});
+
+	ugui_size_t data_size;
+	ugui_font_get_text_size(&font_robotomono_regular_18,
+	                        data,
+	                        &data_size);
+
+	ugui_graphics_draw_text(graphics_ctx,
+	                        data,
+	                        &font_robotomono_regular_18,
+	(ugui_point_t) {
+		.x = w / 2 - data_size.w / 2, .y = title_size.h + 16
+	});
 }
 
 ugui_menu_widget_t *ugui_menu_widget_create(ugui_rect_t bounds) {
@@ -50,7 +83,8 @@ ugui_menu_widget_t *ugui_menu_widget_create(ugui_rect_t bounds) {
 
 	menu_widget->selected = 0;
 
-	menu_widget->callbacks.draw_header = ugui_menu_widget_basic_header_draw;
+	menu_widget->draw_callbacks.draw_header = ugui_menu_widget_basic_header_draw;
+	menu_widget->draw_callbacks.draw_row = ugui_menu_widget_basic_cell_draw;
 
 	ugui_layer_set_update(menu_widget->widget_layer, menu_widget_layer_update);
 
@@ -59,15 +93,19 @@ ugui_menu_widget_t *ugui_menu_widget_create(ugui_rect_t bounds) {
 	return menu_widget;
 }
 
-void ugui_menu_widget_set_callbacks(ugui_menu_widget_t* menu_widget, ugui_menu_widget_callbacks_t* callbacks)
+void ugui_menu_widget_set_callbacks(ugui_menu_widget_t* menu_widget, ugui_menu_widget_data_callbacks_t* callbacks)
 {
-	menu_widget->callbacks.get_num_sections = callbacks->get_num_sections;
-	menu_widget->callbacks.get_num_rows = callbacks->get_num_rows;
-	menu_widget->callbacks.get_header_height = callbacks->get_header_height;
-	menu_widget->callbacks.get_cell_height = callbacks->get_cell_height;
-	menu_widget->callbacks.select = callbacks->select;
-	menu_widget->callbacks.draw_header = callbacks->draw_header;
-	menu_widget->callbacks.draw_row = callbacks->draw_row;
+	menu_widget->data_callbacks.get_num_sections = callbacks->get_num_sections;
+	menu_widget->data_callbacks.get_num_rows = callbacks->get_num_rows;
+	menu_widget->data_callbacks.get_header_height = callbacks->get_header_height;
+	menu_widget->data_callbacks.get_cell_height = callbacks->get_cell_height;
+	menu_widget->data_callbacks.select = callbacks->select;
+}
+
+void ugui_menu_widget_set_draw(ugui_menu_widget_t* menu_widget, ugui_menu_widget_draw_callbacks_t* callbacks) 
+{
+	menu_widget->draw_callbacks.draw_header = callbacks->draw_header;
+	menu_widget->draw_callbacks.draw_row = callbacks->draw_row;
 }
 
 void ugui_menu_widget_attach_to_window(ugui_menu_widget_t* menu_widget, ugui_window_t* window)
