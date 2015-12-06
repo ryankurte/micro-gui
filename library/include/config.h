9bf64e7242e48a92080c54ba127dbfd99ec67544
Ryan Kurte <ryankurte@gmail.com>
2015-12-06 23:07:43 +1300

    Adding config files

---
+++ library/include/config.h
@@ -0,0 +1,15 @@
+
+
+#ifndef UGUI_CONFIG_H
+#define UGUI_CONFIG_H
+
+#include <stdint.h>
+
+#define UGUI_PADDING_VERTICAL	16
+#define UGUI_PADDING_HORIZONTAL	16
+
+struct ugui_config_s {
+	uint8_t menu_height;
+};
+
+#endif
