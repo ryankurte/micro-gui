a277d2b30b11d302ec674fda6f00a26083d82ba2
Ryan Kurte <ryankurte@gmail.com>
2015-12-16 15:10:48 +1300

    This is awful

--- info.sh
+++ info.sh
@@ -3,6 +3,10 @@
 echo "System information"
 echo `uname -a`
 
+echo "Package information"
+echo `ls /etc/`
+echo `ls /etc/apt/`
+
 echo "SDL information"
 echo `pkg-config --cflags sdl2`
 echo `apt-cache search sdl2`
