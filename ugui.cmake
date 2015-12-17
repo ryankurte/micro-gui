eaa7c35b37de9ffa93f568534ba7962ce7ff648e
Ryan Kurte <ryankurte@gmail.com>
2015-12-17 22:20:16 +1300

    Icons now built in by default. Should maybe be separate libraries for each size to minimize size, though optimizer can probably deal to the unused ones. See issue #2

--- ugui.cmake
+++ ugui.cmake
@@ -20,6 +20,14 @@ set(UGUI_FONTS
 	${CMAKE_CURRENT_LIST_DIR}/source/fonts/robotomono_regular_18.c
 	)
 
+set(UGUI_ICONS
+	${CMAKE_CURRENT_LIST_DIR}/source/icons/icons_1x.c
+	${CMAKE_CURRENT_LIST_DIR}/source/icons/icons_2x.c 
+	${CMAKE_CURRENT_LIST_DIR}/source/icons/icons_4x.c 
+	${CMAKE_CURRENT_LIST_DIR}/source/icons/icons_6x.c 
+	${CMAKE_CURRENT_LIST_DIR}/source/icons/icons_8x.c 
+	)
+
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-add_library(libugui ${UGUI_SOURCES} ${UGUI_FONTS} ${UGUI_WIDGETS})
+add_library(libugui ${UGUI_SOURCES} ${UGUI_FONTS} ${UGUI_ICONS} ${UGUI_WIDGETS})
